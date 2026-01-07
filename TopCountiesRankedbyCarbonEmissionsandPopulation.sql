671c5e92aedf36895a3c117ef1f7225ea36de8a6
Aryaman Tomer <101023524+AryamanTomer@users.noreply.github.com>
2026-01-06 21:24:37 -0600

    Add SQL query to rank counties by emissions and population

---
+++ TopCountiesRankedbyCarbonEmissionsandPopulation.sql
@@ -0,0 +1,9 @@
+SELECT county_state_name,
+       population,
+       CAST(
+        REPLACE(`GHG emissions mtons CO2e`,',','')
+        AS DOUBLE
+       ) AS Total_Emissions
+FROM emissions_data
+ORDER BY total_emissions DESC
+LIMIT 10
